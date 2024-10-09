da52f77d8f797ef0b56f83600f1db6115094a8b6
asbillt <davidasbill@asbillt.com>
2024-10-09 17:17:36 -0500

    Remove PressureVessel class and import Classes module

--- Sizing.py
+++ Sizing.py
@@ -1,31 +1,14 @@
 import math
 import decimal
-
-class PressureVessel:
-
-    def __init__(self):
-        pass
-
-    #P: Design Pressure
-    #R: Inside Radius in Inches
-    #S: Material Stress
-    #E: Joint Efficiency
-    #CA: Corrosion Allowance
-    #t: Minimum Required Thickness of Shell
-    def shellMinThickness(self, P, R, S, E, CA = 0):
-        if CA == 0:
-            t = (int(P) * int(R)) / ((int(S) * int(E)) - (0.6 * int(P)))
-        else:
-            t = ((int(P) * (int(R) + float(CA))) / ((int(S) * int(E)) - (0.6 * int(P)))) + float(CA)
-        return round(t, 4)
+import Classes
 
 
-Exxon = PressureVessel()
+Exxon = Classes.PressureVessel()
 P = input("Enter design pressure in psi: ")
 R = input("Enter inside radius in inches: ")
 S = input("Enter allowable material stress in psi: ")
 E = input("Enter joint efficiency: ")
 CA = input("Enter corrosion allowance: ")
-shellMinThickness = Exxon.shellMinThickness(P, R, S, E, CA)
+shellMinThickness = Exxon.shellMinThickness(int(P), int(R), int(S), int(E), float(CA))
 print("The Minimum Required Thickness for the Shell is: " + str(shellMinThickness) + " inches.")
 
