1d7ccec2768afeb69cd40da475169c2dec189aca
asbillt <davidasbill@asbillt.com>
2024-10-21 18:14:45 -0500

    Add outside radius method to PressureVessel class

--- Classes.py
+++ Classes.py
@@ -6,14 +6,41 @@ class PressureVessel:
         pass
 
     #P: Design Pressure
-    #R: Inside Radius in Inches
+    #IR: Inside Radius in Inches
+    #OR: Outside Radius in Inches
     #S: Material Stress
     #E: Joint Efficiency
     #CA: Corrosion Allowance
     #t: Minimum Required Thickness of Shell
-    def shellMinThickness(self, P, R, S, E, CA = 0):
+
+    #Calculate shell minimum thickness in terms of Inside Radius
+    def shellMinThicknessIR(self, P, IR, S, E, CA = 0):
         if CA == 0:
-            t = (P * R) / ((S * E) - (0.6 * P))
+            t = (P * IR) / ((S * E) - (0.6 * P))
         else:
-            t = ((P * (R + CA)) / ((S * E) - (0.6 * P))) + CA
+            t = ((P * (IR + CA)) / ((S * E) - (0.6 * P))) + CA
         return round(t, 4)
+
+    #Calculate shell minimum thickness in terms of Outside Radius
+    def shellMinThicknessOR(self, P, OR, S, E, CA = 0):
+        if CA == 0:
+            t = (P * OR) / ((S * E) + (0.4 * P))
+        else:
+            t = ((P * OR) / ((S * E) - (0.6 * P))) + CA
+        return round(t, 4)
+
+
+class Pump:
+    
+    def __init__(self):
+        pass
+
+class HeatExchanger:
+    
+    def __init__(self):
+        pass
+
+class PSV:
+    
+    def __init__(self):
+        pass
--- Sizing.py
+++ Sizing.py
@@ -2,13 +2,30 @@ import math
 import decimal
 import Classes
 
+userInput = input("What equipment do you want to size?\n"
+                  "Type \"PV\" for Pressure Vessel sizing\n"
+                  "Type \"P\" for Pump sizing\n"
+                  "Type \"HEX\" Heat Exchanger sizing\n"
+                  "Type \"PSV\" for Pressure Safety Valve sizing\n")
 
-Exxon = Classes.PressureVessel()
-P = input("Enter design pressure in psi: ")
-R = input("Enter inside radius in inches: ")
-S = input("Enter allowable material stress in psi: ")
-E = input("Enter joint efficiency: ")
-CA = input("Enter corrosion allowance: ")
-shellMinThickness = Exxon.shellMinThickness(int(P), int(R), int(S), int(E), float(CA))
-print("The Minimum Required Thickness for the Shell is: " + str(shellMinThickness) + " inches.")
+if userInput == "PV":
+    PressureVessel = Classes.PressureVessel()
+    user_input_pv = input("What calculation do you want to run?\n"
+                          "Type \"minthickIR\" for UG-27(c)(1) Minimum Required Thickness of Shell under Internal Pressure in terms of Inside Radius\n"
+                          "Type \"minthickOR\" for Mandatory Appendix 1-1 Minimum Required Thickness of Shell under Internal Pressure in terms of Outside Radius\n")
+    if user_input_pv == "minthickIR":
+        P = input("Enter design pressure in psi: ")
+        R = input("Enter inside radius in inches: ")
+        S = input("Enter allowable material stress in psi: ")
+        E = input("Enter joint efficiency: ")
+        CA = input("Enter corrosion allowance: ")
+        shellMinThickness = PressureVessel.shellMinThicknessIR(int(P), int(R), int(S), int(E), float(CA))
+    elif user_input_pv == "minthickOR":
+        P = input("Enter design pressure in psi: ")
+        R = input("Enter outside radius in inches: ")
+        S = input("Enter allowable material stress in psi: ")
+        E = input("Enter joint efficiency: ")
+        CA = input("Enter corrosion allowance: ")
+        shellMinThickness = PressureVessel.shellMinThicknessOR(int(P), int(R), int(S), int(E), float(CA))
+    print("The Minimum Required Thickness for the Shell is: " + str(shellMinThickness) + " inches.")
 
